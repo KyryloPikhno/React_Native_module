11bc50fa36ccd12bf9a51bf574b061e697be73d6
KyryloPikhno <Mr.Good@i.ua>
2023-05-20 15:05:44 +0300

    explore routing

---
+++ screens/Home.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import {Button, Text, View} from 'react-native';
+import {useNavigation} from '@react-navigation/native';
+import {HomeNavigationProps, HomeRoutes} from '../routes/HomeRoute';
+
+export const Home = () => {
+  const navigation = useNavigation<HomeNavigationProps>();
+  return (
+    <View
+      style={{
+        height: '100%',
+        alignItems: 'center',
+        justifyContent: 'center',
+      }}>
+      <Text>Home Screen</Text>
+      <Button
+        title={'Go to Posts screen'}
+        onPress={() => navigation.navigate(HomeRoutes.Posts, {postId: '1'})}
+      />
+    </View>
+  );
+};
