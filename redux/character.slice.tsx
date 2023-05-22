ac91ea4e5c26bb944c4ce728c8b462be1e0b3c39
KyryloPikhno <Mr.Good@i.ua>
2023-05-22 19:08:36 +0300

    configure slice

---
+++ redux/character.slice.tsx
@@ -0,0 +1,52 @@
+import {createSlice, createAsyncThunk} from '@reduxjs/toolkit';
+import {fetchCharacter} from '../services/character.service';
+
+interface ICharacter {
+  id: number;
+  name: string;
+  status: string;
+  image: string;
+}
+
+interface IInitialState {
+  characters: ICharacter[];
+  loading: boolean;
+  error: string | null;
+}
+
+const initialState: IInitialState = {
+  characters: [],
+  loading: false,
+  error: null,
+};
+
+export const getAll = createAsyncThunk(
+  'characters/fetchCharacters',
+  async () => {
+    const {data} = await fetchCharacter();
+    return data;
+  },
+);
+
+const charactersSlice = createSlice({
+  name: 'characters',
+  initialState,
+  reducers: {},
+  extraReducers: builder => {
+    builder
+      .addCase(getAll.pending, state => {
+        state.loading = true;
+        state.error = null;
+      })
+      .addCase(getAll.fulfilled, (state, action) => {
+        state.loading = false;
+        state.characters = action.payload;
+      })
+      .addCase(getAll.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.error.message || 'Failed to fetch characters.';
+      });
+  },
+});
+
+export default charactersSlice.reducer;
